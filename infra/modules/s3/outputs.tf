ebde3f3f234bcdaf5c4d954e2dcad16dcafd38c5
victorhts <victor.hts.dev@gmail.com>
2024-12-21 17:27:15 -0300

    teste terraform

--- infra/modules/s3/outputs.tf
+++ infra/modules/s3/outputs.tf
@@ -12,8 +12,3 @@ output "bucket_prefix" {
   description = "Prefixo do bucket S3"
   value       = var.prefix
 }
-
-output "bucket_arn" {
-  description = "ARN do bucket S3"
-  value       = aws_s3_bucket.bucket.arn
-}
