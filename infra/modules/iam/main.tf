6e991f74c9085977eac7703f1869743667331816
victorhts <victor.hts.dev@gmail.com>
2024-12-21 15:59:58 -0300

    teste destroy

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -1,15 +1,18 @@
 data "aws_caller_identity" "current" {}
 
+# Usuário tech-admin
 resource "aws_iam_user" "tech_admin_user" {
   name = var.user_name
 }
 
+# Membership do grupo Admins
 resource "aws_iam_group_membership" "admins_membership" {
   name  = "admins-membership"
   group = var.group_name
   users = [aws_iam_user.tech_admin_user.name]
 }
 
+# Role techRole
 resource "aws_iam_role" "tech_role" {
   name = var.role_name
 
@@ -27,7 +30,7 @@ resource "aws_iam_role" "tech_role" {
   })
 }
 
-
+# Política associada à techRole
 resource "aws_iam_policy" "tech_role_policy" {
   name        = var.role_policy_name
   description = "Permite acesso genérico ao S3 (será atualizado após criação do bucket)"
@@ -44,7 +47,14 @@ resource "aws_iam_policy" "tech_role_policy" {
   })
 }
 
+
 resource "aws_iam_role_policy_attachment" "attach_s3_policy" {
   role       = aws_iam_role.tech_role.name
   policy_arn = aws_iam_policy.tech_role_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "detach_s3_policy" {
+  depends_on = [
+    aws_iam_role_policy_attachment.attach_s3_policy
+  ]
 }
