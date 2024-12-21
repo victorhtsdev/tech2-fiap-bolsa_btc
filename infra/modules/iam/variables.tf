1cd094288ee48ba8f5319995b0faa486b5bbd6b5
victorhts <victor.hts.dev@gmail.com>
2024-12-21 15:14:10 -0300

    Ajustes terraform

--- infra/modules/iam/variables.tf
+++ infra/modules/iam/variables.tf
@@ -1,24 +1,29 @@
 variable "user_name" {
   description = "Nome do usuário IAM"
   type        = string
+  default     = "tech-admin"
 }
 
 variable "group_name" {
   description = "Nome do grupo IAM"
   type        = string
+  default     = "Admins"
 }
 
 variable "role_name" {
   description = "Nome da Role IAM"
   type        = string
+  default     = "techRole"
 }
 
 variable "role_policy_name" {
   description = "Nome da política da Role IAM"
   type        = string
+  default     = "TechRoleS3AccessPolicy"
 }
 
 variable "bucket_name" {
   description = "Nome do bucket S3"
   type        = string
+  default     = "vhts-fiap-tech-challenge2"
 }
--- infra/modules/s3/variables.tf
+++ infra/modules/s3/variables.tf
@@ -1,14 +1,17 @@
 variable "bucket_name" {
   description = "Nome do bucket S3"
   type        = string
+  default     = "vhts-fiap-tech-challenge2"
 }
 
 variable "prefix" {
   description = "Prefixo da estrutura dentro do bucket"
   type        = string
+  default     = "bolsa_bovespa/raw"
 }
 
 variable "region" {
   description = "Região do bucket S3"
   type        = string
+  default     = "us-east-1"
 }
