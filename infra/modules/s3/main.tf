aa3619c3f2e17458f0bcd503c9132b86689c65c8
victorhts <victor.hts.dev@gmail.com>
2024-12-21 16:43:28 -0300

    teste terraform

--- infra/modules/s3/main.tf
+++ infra/modules/s3/main.tf
@@ -11,6 +11,15 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = false
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.bucket.id
 
@@ -33,19 +42,18 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
         }
       },
       {
-
+        # Permitir acesso ao IAM role
         Effect = "Allow",
-        Principal = "*",
-        Action    = ["s3:GetObject", "s3:PutObject", "s3:ListBucket"],
-        Resource  = [
+        Principal = {
+          AWS = [
+            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/techRole"
+          ]
+        },
+        Action = ["s3:GetObject", "s3:PutObject", "s3:ListBucket"],
+        Resource = [
           "${aws_s3_bucket.bucket.arn}",
           "${aws_s3_bucket.bucket.arn}/*"
-        ],
-        Condition = {
-          StringEquals: {
-            "aws:PolicyArn": var.role_policy_name
-          }
-        }
+        ]
       }
     ]
   })
